660cfbeade8e617025ae75064a4b72dc56753ca6
Maksym-Semenov <mestcomua@gmail.com>
2022-06-19 22:24:18 +0300

    Добавьте файлы проекта.

---
+++ QuadraticEquationByDelegate/Program.cs
@@ -0,0 +1,49 @@
+﻿using QuadraticEquationByDelegate;
+
+while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Эта программа решает квадратные уравнения вида ax2+bx+c=0");
+        Console.Write("Введите первый коэффициент A, не равный 0: ");
+
+        if (int.TryParse(Console.ReadLine(), out int a) && a != 0)
+            _ = a;
+        else
+        {
+            Console.WriteLine("Введите не 0");
+            Console.ReadLine();
+            continue;
+        }
+        Console.Write("Введите второй коэффициент B, не равный 0: ");
+        if (int.TryParse(Console.ReadLine(), out int b) && b != 0)
+            _ = b;
+        else
+        {
+            Console.WriteLine("Введите не 0");
+            Console.ReadLine();
+            continue;
+        }
+        Console.Write("Введите третий коэффициент C, не равный 0: ");
+        if (int.TryParse(Console.ReadLine(), out int c) && c != 0)
+            _ = c;
+        else
+        {
+            Console.WriteLine("Введите не 0");
+            Console.ReadLine();
+            continue;
+        }
+    Root root = new Root();
+    RootEquation result = root.GetRoot;
+
+
+
+
+        Console.WriteLine(Test(result, a, b, c));
+        Console.ReadLine();
+
+    static string Test(RootEquation method, int a, int b, int c)
+    {
+        return method(a, b, c);
+}
+}
+
---
+++ QuadraticEquationByDelegate/Root.cs
@@ -0,0 +1,25 @@
+﻿
+namespace QuadraticEquationByDelegate
+{
+    public delegate string RootEquation(int a, int b, int c);
+    internal class Root
+    {
+        public string GetRoot(int a, int b, int c)
+        {
+            double Diskriminant = Math.Pow(b, 2) - (4 * a * c);
+            if (Diskriminant < 0)
+            {
+                double X1 = (-b - Math.Sqrt(Diskriminant)) / (2 * a);
+                double X2 = (-b + Math.Sqrt(Diskriminant)) / (2 * a);
+                return "Уравнение имеет два корня: X1 = " + X1 + " и X2 = " + X2 ;
+            }
+            else if (Diskriminant == 0)
+            {
+                double X = (-b ) / (2 * a);
+                return "Уравнение имеет один корень X = " + X ;
+            }
+            else
+                return "Уравнение не имеет корней";
+        }
+    }
+}
